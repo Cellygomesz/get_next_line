dededfe7a086f747e99fec5b95bc25d3c946d613
mgomes-s <marcellygomes003@gmail.com>
2024-10-22 14:04:12 -0300

    22/10

--- get_next_line.c
+++ get_next_line.c
@@ -3,41 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgomes-s <mgomes-s@42.rio>                 +#+  +:+       +#+        */
+/*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:24:28 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/10/22 13:06:19 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2024/10/22 14:02:49 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "get_next_line.h"
-/*
+
+void	create_list(t_list **list, int fd)
+{
+	
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
-	int	i = 0;
-	
-	buffer = (char *)malloc(sizeof(char *));
-	if (!buffer)
-		return ;
+	static t_list	*list = NULL;
+	char			*next_line;
 
-	while (buffer[0] != '\n')
-	{
-		read(fd, buffer, 1);
-		printf("%s", buffer);
-		i++;
-	}
-	return (buffer);
-}
-*/
-int	main(void)
-{
-	int	fd = open("test.txt", O_RDONLY | O_CREAT);
-	//get_next_line(fd);
-	printf("fd do arquivo: %d\n", fd);
-	return (0);
+	if (fd < 0 || read(fd, &next_line, 0) < 0||BUFFER_SIZE <= 0)
+		return (NULL);
+	create_list(&list, fd);
+	if (list == NULL)
+		return (NULL);
+	next_line = get_line(list);
+	polish_list(&list);
+	return (next_line);
 }
