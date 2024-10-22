e1e49d7a8eed32c0efbd599669669730bb0248b0
mgomes-s <marcellygomes003@gmail.com>
2024-10-22 12:00:08 -0300

    22/10

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,37 @@
 /*   By: mgomes-s <mgomes-s@42.rio>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:24:28 by mgomes-s          #+#    #+#             */
-/*   Updated: 2024/10/21 15:11:53 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2024/10/22 11:05:17 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
+	int	i = 0;
+	
+	buffer = (char *)malloc(sizeof(char *));
+	if (!buffer)
+		return ;
+
+	while (buffer[0] != '\n')
+	{
+		read(fd, buffer, 1);
+		printf("%s", buffer);
+		i++;
+	}
+	return (buffer);
+}
+
+int	main(void)
+{
+	int	fd = open("test.txt", O_RDONLY);
+	get_next_line(fd);
+	return (0);
 }
