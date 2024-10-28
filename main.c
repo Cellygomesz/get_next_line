b4eb4efd06b577ea279bd9372c095c9a6ee20ee5
mgomes-s <marcellygomes003@gmail.com>
2024-10-28 10:18:34 -0300

    28/10

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgomes-s <mgomes-s@42.rio>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 10:14:55 by mgomes-s          #+#    #+#             */
+/*   Updated: 2024/10/28 10:16:10 by mgomes-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-	int	fd = open("test.txt",  O_RDONLY);
-	
-	get_next_line(fd);
+	int		fd;
+	char	*frase;
+
+	fd = open("test.txt", O_RDONLY);
+	frase = get_next_line(fd);
+	while (frase)
+	{
+		printf("%s", frase);
+		free(frase);
+		frase = get_next_line(fd);
+	}
 	close(fd);
 	return (0);
 }
